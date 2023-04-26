fec3fe1c8bd11571068ee34f80f3645c25ae7da2
Abdulkodir Yusupov <44323682+nvrbckdown@users.noreply.github.com>
2023-04-26 18:06:44 +0500

    action edited

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask, request, jsonify
 import json
+import os
 
 app=Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def default():
+    res = {
+		'string': 'Udevs DevOps Bootcamp'
+	}
+    return jsonify(res)
 
 @app.route('/project', methods=['GET'])
 def get_project():
